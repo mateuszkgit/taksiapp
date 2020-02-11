2fc20649af853a7ecde89ca738ab89a2d7634094
mateuszkgit <matteoo1000@o2.pl>
2020-02-11 13:09:52 +0100

    app

---
+++ app/taxi/taxi/routing.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from taxi.middleware import TokenAuthMiddlewareStack
+from trips.consumers import TaxiConsumer
+
+application = ProtocolTypeRouter({
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter([
+            path('taxi/', TaxiConsumer),
+        ])
+    ),
+})
